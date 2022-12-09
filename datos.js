3e1f2c39f43e1038855a508d3ebdb042e9b6e54e
Benji_rios <benji_rva@hotmail.com>
2022-12-08 20:20:19 -0600

    Actualizacion

--- datos.js
+++ datos.js
@@ -1,1 +1,26 @@
-let myName = "Benja";
+let myName = "Benja"; //Estoy guardando un dato de tipo string
+let myAge = 26; //Esta variable guarda un dato de tipo númerico
+let isStudent = false //Esta variable guarda un dato booleano
+let isTeacher = true //Esta variable guarda un dato booleano
+let currentYear = null //Aquí no estoy especificando un valor en concreto
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isTeacher);
+console.log(currentYear);
+
+
+
+console.log(`Mi nombre es ${myName} y tengo ${myAge} y soy estudiante? ${isStudent}`)
+
+
+let aboutMe = {
+    name:  "Benja",
+    lastName: "Ricardo",
+}
+
+console.log(aboutMe)
+
+
+let favoriteFoods ={"Carne", "pasta", "tacos"}
